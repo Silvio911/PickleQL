1355681795e246fba09855441321cdadb2e7c798
silvio911 <silviobulla12@gmail.com>
2025-03-23 21:44:08 +0100

    Initial Commit

---
+++ PickleQLTests/PickleQLTests.swift
@@ -0,0 +1,17 @@
+//
+//  PickleQLTests.swift
+//  PickleQLTests
+//
+//  Created by Silvio Bulla on 23.03.25.
+//
+
+import Testing
+@testable import PickleQL
+
+struct PickleQLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
